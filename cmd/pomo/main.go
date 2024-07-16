3b7b7fef512f6364ec6defa814b0b077006fde86
Max Celant <maxcelant@gmail.com>
2024-07-16 11:45:40 -0700

    simple state management

--- cmd/pomo/main.go
+++ cmd/pomo/main.go
@@ -6,29 +6,47 @@ import (
 	"time"
 )
 
+type State int
+
+const (
+  INIT State = iota
+  ACTIVE
+  BREAK
+  DONE
+)
+
 var activeTime int = 25
 var breakTime int = 5
-var intervals int = 0
+var intervals int = 3 
 
 func usage() {
 	fmt.Println("Usage: pomo [command]")
 }
 
 func timer(timeAmount int) {
-  for t := 0; t < timeAmount; t++ {
-    select {
-    case <- time.After(1 * time.Second):
-      fmt.Printf("%d remaining\n", timeAmount - t)
-    }
-  }
+	for t := 0; t < timeAmount; t++ {
+		select {
+		case <-time.After(1 * time.Second):
+			fmt.Printf("%d remaining\n", timeAmount-t)
+		}
+	}
 }
 
 func handleStartCommand(subcommands []string) {
+  curState := INIT
+
 	for _, s := range subcommands {
 		fmt.Printf("%s\n", s)
 	}
-
-	timer(activeTime)
+  
+  for i := 0; i < intervals; i++ {
+    curState = ACTIVE
+    fmt.Printf("State is now active...%d\n", curState)
+	  timer(activeTime)
+    curState = BREAK
+    fmt.Printf("State is now break...%d\n", curState)
+	  timer(breakTime)
+  }
 }
 
 func main() {
