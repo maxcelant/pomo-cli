dd64057ff1170bf189c7f01128d353c36289c909
Max Celant <maxcelant@gmail.com>
2024-07-16 11:35:27 -0700

    simple non blocking timer

--- cmd/pomo/main.go
+++ cmd/pomo/main.go
@@ -1,8 +1,48 @@
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+var activeTime int = 25
+var breakTime int = 5
+var intervals int = 0
+
+func usage() {
+	fmt.Println("Usage: pomo [command]")
+}
+
+func timer(timeAmount int) {
+  for t := 0; t < timeAmount; t++ {
+    select {
+    case <- time.After(1 * time.Second):
+      fmt.Printf("%d remaining\n", timeAmount - t)
+    }
+  }
+}
+
+func handleStartCommand(subcommands []string) {
+	for _, s := range subcommands {
+		fmt.Printf("%s\n", s)
+	}
+
+	timer(activeTime)
+}
 
 func main() {
-  fmt.Println("Usage: pomo [command]")
+	args := os.Args
+
+	if len(args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	for _, arg := range args[1:] {
+		if arg == "start" {
+			handleStartCommand(args[2:])
+			break
+		}
+	}
 }
