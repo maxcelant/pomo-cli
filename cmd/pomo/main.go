b985d14b9d253f1626b92ac2559ecfcf61c92ad0
Max Celant <maxcelant@gmail.com>
2024-07-17 09:08:11 -0700

    basic layout

--- cmd/pomo/main.go
+++ cmd/pomo/main.go
@@ -2,51 +2,107 @@ package main
 
 import (
 	"fmt"
+  "bufio"
 	"os"
 	"time"
+  "strings"
 )
 
 type State int
 
 const (
-  INIT State = iota
-  ACTIVE
-  BREAK
-  DONE
+	INIT State = iota
+	ACTIVE
+	BREAK
+  WAITING
+	DONE
 )
 
 var activeTime int = 25
 var breakTime int = 5
-var intervals int = 3 
+var intervals int = 3
 
 func usage() {
 	fmt.Println("Usage: pomo [command]")
 }
 
-func timer(timeAmount int) {
+func getStateRepr(s State) string {
+	if s == 0 {
+		return "Initial"
+	} else if s == 1 {
+		return "Active"
+	} else if s == 2 {
+		return "Break"
+	} else if s == 3 {
+    return "Waiting"
+  }
+	return "Done"
+}
+
+func getStateSymbol(s State) string {
+	if s == 0 {
+		return "🔵"
+	} else if s == 1 {
+		return "🟢"
+	} else if s == 2 {
+		return "🔴"
+	} else if s == 3 {
+    return "🟣"
+  }
+	return "⚫️"
+}
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
+
+func timer(timeAmount int, curState State, interval int) {
+	stateRepr := getStateRepr(curState)
+  stateSymbol := getStateSymbol(curState)
 	for t := 0; t < timeAmount; t++ {
 		select {
 		case <-time.After(1 * time.Second):
-			fmt.Printf("%d remaining\n", timeAmount-t)
+			clearScreen()
+			fmt.Println("🍎 Time to focus")
+			fmt.Printf("   State: %s %s\n", stateRepr, stateSymbol)
+			fmt.Printf("   Interval: %d\n", interval)
+			fmt.Printf("   Time Remaining: %ds\n", timeAmount-t)
 		}
 	}
 }
 
+func waitUserResponse(s State) {
+  clearScreen()
+  if s == ACTIVE {
+    fmt.Printf("🍎 Active session done! Ready to start break?")
+  } else {
+    fmt.Printf("🍎 Break session done! Ready to start studying?")
+  }
+  fmt.Printf("   [Enter] to continue, or [Q]uit: ")
+	reader := bufio.NewReader(os.Stdin)
+  input, _ := reader.ReadString('\n')
+  input = strings.TrimSpace(input)
+  if input == "q" || input == "Q" {
+    fmt.Printf("  Exiting gracefully...")
+    os.Exit(0)
+  }
+}
+
 func handleStartCommand(subcommands []string) {
-  curState := INIT
+	curState := INIT
 
 	for _, s := range subcommands {
 		fmt.Printf("%s\n", s)
 	}
-  
-  for i := 0; i < intervals; i++ {
-    curState = ACTIVE
-    fmt.Printf("State is now active...%d\n", curState)
-	  timer(activeTime)
-    curState = BREAK
-    fmt.Printf("State is now break...%d\n", curState)
-	  timer(breakTime)
-  }
+
+	for i := 0; i < intervals; i++ {
+		curState = ACTIVE
+		timer(activeTime, curState, i)
+    waitUserResponse(curState)
+		curState = BREAK
+		timer(breakTime, curState, i)
+    waitUserResponse(curState)
+	}
 }
 
 func main() {
@@ -57,10 +113,10 @@ func main() {
 		os.Exit(1)
 	}
 
-  switch args[1] {
-    case "start": 
-      handleStartCommand(args[2:])
-    default:
-      usage()
-  }
+	switch args[1] {
+	case "start":
+		handleStartCommand(args[2:])
+	default:
+		usage()
+	}
 }
--- docs/notes.md
+++ docs/notes.md
@@ -1,3 +1,5 @@
 ### Notes
 
 - Might need a state manager, something to handle what the current state is: init, active, break, quit
+- it should definitely have a state and a stringified version of that state. Maybe call it repr
+- `State` should also be a struct with the `type`, `repr`, and `symbol`
