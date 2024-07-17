71bc93aea92c6ffa804af381aca770bc17690cde
Max Celant <maxcelant@gmail.com>
2024-07-17 10:46:08 -0700

    updates to state pkg

--- cmd/state/state.go
+++ cmd/state/state.go
@@ -1,42 +1,37 @@
-
 package state
 
 type ID int
 
 const (
-  INIT ID = iota
-  ACTIVE
-  BREAK
-  WAITING
-  PAUSE
-  DONE
+	INIT ID = iota
+	ACTIVE
+	BREAK
+	WAITING
+	PAUSE
+	DONE
 )
 
 type State struct {
-  Id ID 
-  Literal string
-  Symbol string
+	Id      ID
+	Literal string
+	Symbol  string
 }
 
 func New(id ID, literal string, symbol string) State {
-  return State{
-    Id:id,
-    Literal: literal,
-    Symbol: symbol,
-  }
+	return State{
+		Id:      id,
+		Literal: literal,
+		Symbol:  symbol,
+	}
 }
 
-var States map[ID]State
-
-func init() {
-	States = map[ID]State{
-		INIT:    New(INIT, "Initial", "🔵"),
-		ACTIVE:  New(ACTIVE, "Active", "🟢"),
-		BREAK:   New(BREAK, "Break", "🔴"),
-		WAITING: New(WAITING, "Waiting", "⏱️"),
-		PAUSE:   New(PAUSE, "Pause", "⏸️"),
-		DONE:    New(DONE, "Done", "✅"),
-	}
+var States = map[ID]State{
+	INIT:    New(INIT, "Initial", "🔵"),
+	ACTIVE:  New(ACTIVE, "Active", "🟢"),
+	BREAK:   New(BREAK, "Break", "🔴"),
+	WAITING: New(WAITING, "Waiting", "⏱️"),
+	PAUSE:   New(PAUSE, "Pause", "⏸️"),
+	DONE:    New(DONE, "Done", "✅"),
 }
 
 func Get(id ID) State {
--- cmd/state/state_test.go
+++ cmd/state/state_test.go
@@ -5,21 +5,21 @@ import (
 )
 
 func TestNewState(t *testing.T) {
-  id := INIT 
-  literal := "active"
-  symbol := "🟢"
-  
-  state := New(id, literal, symbol)
-  
-  if state.Id != id {
-    t.Errorf("Expected id %d, but got %d", id, state.Id)
-  }
-  
-  if state.Literal != literal {
-    t.Errorf("Expected literal %s, but got %s", literal, state.Literal)
-  }
-  
-  if state.Symbol != symbol {
-    t.Errorf("Expected symbol %s, but got %s", symbol, state.Symbol)
-  }
+	id := INIT
+	literal := "active"
+	symbol := "🟢"
+
+	state := New(id, literal, symbol)
+
+	if state.Id != id {
+		t.Errorf("Expected id %d, but got %d", id, state.Id)
+	}
+
+	if state.Literal != literal {
+		t.Errorf("Expected literal %s, but got %s", literal, state.Literal)
+	}
+
+	if state.Symbol != symbol {
+		t.Errorf("Expected symbol %s, but got %s", symbol, state.Symbol)
+	}
 }
