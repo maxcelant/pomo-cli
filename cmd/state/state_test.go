42cbe5bb58f1bcbac2ce3a5745807c2ee2944bbc
Max Celant <maxcelant@gmail.com>
2024-07-17 10:43:05 -0700

    fix test

--- cmd/state/state_test.go
+++ cmd/state/state_test.go
@@ -5,21 +5,21 @@ import (
 )
 
 func TestNewState(t *testing.T) {
-  id := state.INIT
+  id := INIT 
   literal := "active"
   symbol := "🟢"
   
   state := New(id, literal, symbol)
   
-  if state.id != id {
-    t.Errorf("Expected id %d, but got %d", id, state.id)
+  if state.Id != id {
+    t.Errorf("Expected id %d, but got %d", id, state.Id)
   }
   
-  if state.literal != literal {
-    t.Errorf("Expected literal %s, but got %s", literal, state.literal)
+  if state.Literal != literal {
+    t.Errorf("Expected literal %s, but got %s", literal, state.Literal)
   }
   
-  if state.symbol != symbol {
-    t.Errorf("Expected symbol %s, but got %s", symbol, state.symbol)
+  if state.Symbol != symbol {
+    t.Errorf("Expected symbol %s, but got %s", symbol, state.Symbol)
   }
 }
