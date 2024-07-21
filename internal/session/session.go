769869fd6bdf2e09ec5d62f88430227aa189c231
Max Celant <maxcelant@gmail.com>
2024-07-21 14:31:01 -0500

    working pause, but needs work

--- internal/session/session.go
+++ internal/session/session.go
@@ -1,13 +1,16 @@
 package session
 
-import "fmt"
-import "os"
-import "bufio"
-import "strings"
-import "github.com/maxcelant/pomo-cli/internal/manager"
-import "github.com/maxcelant/pomo-cli/internal/timer"
-import "github.com/maxcelant/pomo-cli/internal/state"
-import "github.com/maxcelant/pomo-cli/internal/screen"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/maxcelant/pomo-cli/internal/manager"
+	"github.com/maxcelant/pomo-cli/internal/screen"
+	"github.com/maxcelant/pomo-cli/internal/state"
+	"github.com/maxcelant/pomo-cli/internal/timer"
+)
 
 type TimerCallback func(currentTime int)
 
@@ -15,7 +18,7 @@ type Session struct {
 	manager.StateManager
 	timer     timer.Timer
 	intervals int
-  options   map[string]interface{}
+	options   map[string]interface{}
 }
 
 func New(sm manager.StateManager, t timer.Timer, i int) *Session {
@@ -23,7 +26,7 @@ func New(sm manager.StateManager, t timer.Timer, i int) *Session {
 }
 
 func (s *Session) Start(options map[string]interface{}) {
-  s.options = options
+	s.options = options
 	for {
 		s.loop(state.ACTIVE)
 		s.loop(state.REST)
@@ -32,7 +35,7 @@ func (s *Session) Start(options map[string]interface{}) {
 }
 
 func (s Session) handleInput(inputChan chan<- string) {
-  reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 	for {
 		input, _ := reader.ReadString('\n')
 		inputChan <- strings.TrimSpace(input)
@@ -40,58 +43,61 @@ func (s Session) handleInput(inputChan chan<- string) {
 }
 
 func (s Session) loop(nextState state.ID) {
-  inputChan := make(chan string)
-  go s.handleInput(inputChan)
+	inputChan := make(chan string)
+	controlChan := make(chan struct{})
+	go s.handleInput(inputChan)
 
-  s.UpdateState(state.Get(nextState))
+	s.UpdateState(state.Get(nextState))
 	s.timer.SetDuration(s.State.Duration)
 
-	s.Time(nextState, inputChan, func(t int) {
-    s.PromptText(t)
-	})
+	s.Time(nextState, inputChan, controlChan)
 	s.awaitUserResponse()
 }
 
-func (s *Session) Time(nextState state.ID, inputChan chan string, cb TimerCallback) {
+func (s *Session) Time(nextState state.ID, inputChan chan string, controlChan chan struct{}) {
 	out := make(chan int)
 
-	go s.timer.Countdown(out)
+	go s.timer.Countdown(out, controlChan)
 
 	for {
 		select {
-    case input := <-inputChan:
-      if input == "" && s.State.Id == state.PAUSE {
-        s.UpdateState(state.Get(nextState))
-      } else if input == "" && s.State.Id != state.PAUSE {
-        s.UpdateState(state.Get(state.PAUSE))
-      }
+		case input := <-inputChan:
+			if input == "" && s.State.Id == state.PAUSE {
+				s.UpdateState(state.Get(nextState))
+				controlChan <- struct{}{}
+			} else if input == "" && s.State.Id != state.PAUSE {
+				s.UpdateState(state.Get(state.PAUSE))
+				screen.Clear()
+				fmt.Printf("🍎 Timer paused!\nPress [Enter] to unpause: ")
+				controlChan <- struct{}{}
+			}
 		case time, ok := <-out:
 			if !ok {
 				return
 			}
-			cb(time)
+			s.PromptText(time)
 		}
 	}
 }
 
 // todo: refactor this
 func (s Session) PromptText(t int) {
-  screen.Clear()
-  min, sec := s.timer.FormatDuration(s.State.Duration-t)
-  if s.State.Id == state.PAUSE {
-    fmt.Printf("🍎 Timer paused!\nPress [Enter] to unpause: ")
-    return
-  }
-  if s.options["minimal"] == true {
-    fmt.Printf("🍎 State: %s %s %d:%d\n", s.State.Literal, s.State.Symbol, min, sec)
-    fmt.Printf("   Press [Enter] to pause timer: ")
-    return 
-  }
-  fmt.Println("🍎 Time to focus")
-  fmt.Printf("   State: %s %s\n", s.State.Literal, s.State.Symbol)
-  fmt.Printf("   Interval: %d\n", s.intervals)
-  fmt.Printf("   Time Remaining: %dm %ds\n", min, sec)
-  fmt.Printf("   Press [Enter] to pause timer: ")
+	screen.Clear()
+	min, sec := s.timer.FormatDuration(s.State.Duration - t)
+	if s.State.Id == state.PAUSE {
+		fmt.Printf("🍎 Timer paused!\nPress [Enter] to unpause: ")
+		return
+	}
+	if s.options["minimal"] == true {
+		fmt.Printf("🍎 State: %s %s %d:%d\n", s.State.Literal, s.State.Symbol, min, sec)
+		fmt.Printf("   Press [Enter] to pause timer: ")
+		return
+	}
+	fmt.Println("🍎 Time to focus")
+	fmt.Printf("   State: %s %s\n", s.State.Literal, s.State.Symbol)
+	fmt.Printf("   Interval: %d\n", s.intervals)
+	fmt.Printf("   Time Remaining: %dm %ds\n", min, sec)
+	fmt.Printf("   Press [Enter] to pause timer: ")
 }
 
 func (s *Session) awaitUserResponse() {
--- internal/timer/timer.go
+++ internal/timer/timer.go
@@ -2,7 +2,6 @@ package timer
 
 import "time"
 
-
 type Timer struct {
 	duration int
 }
@@ -11,23 +10,24 @@ func New() Timer {
 	return Timer{0}
 }
 
-func (t Timer) Countdown(out chan<- int) {
+func (t Timer) Countdown(out chan<- int, controlChan chan struct{}) {
 	for i := 0; i < t.duration; i++ {
 		select {
 		case <-time.After(1 * time.Second):
 			out <- i
+		case <-controlChan:
+			<-controlChan
 		}
 	}
 	close(out)
 }
-
 
 func (t *Timer) SetDuration(duration int) {
 	t.duration = duration
 }
 
 func (t Timer) FormatDuration(duration int) (int, int) {
-  minutes := duration / 60
-  seconds := duration % 60
-  return minutes, seconds
+	minutes := duration / 60
+	seconds := duration % 60
+	return minutes, seconds
 }
